9b393f3a529e0dc75273162cfc2c8b715977b0a5
Omraj <omraj7413@gmail.com>
2022-09-18 11:41:29 +0530

    Reports done

--- src/components/Page/Tabs/Reports.js
+++ src/components/Page/Tabs/Reports.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../../UI/page/report/Header'
+import Report from '../../UI/page/report/Report'
 
 function Reports() {
   return (
-    <div>Reports</div>
+    <div className='container'>
+      <Header />
+      <Report />
+    </div>
   )
 }
 
---
+++ src/components/UI/page/report/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style/Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        Reports
+    </div>
+  )
+}
+
+export default Header
